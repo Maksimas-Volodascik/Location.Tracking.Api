3cc7e460edd13758ef2db01359278fca80bbee44
max <maksimas.volodascik@gmail.com>
2026-04-09 20:23:19 +0300

    fear(repository): add base repository with frequently used functions

--- src/Location.Tracking.Infrastructure/DependencyInjection.cs
+++ src/Location.Tracking.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Location.Tracking.Application.Interfaces.Repositories;
+using Location.Tracking.Application.Interfaces.Services;
+using Location.Tracking.Application.Services;
+using Location.Tracking.Infrastructure.Data;
+using Location.Tracking.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Location.Tracking.Infrastructure.Data;
 
 namespace Location.Tracking.Infrastructure
 {
@@ -17,7 +21,12 @@ namespace Location.Tracking.Infrastructure
             services.AddDbContext<TrackingDbContext>(options =>
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
-            
+            //Repositories
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+            //Services
+            services.AddScoped<IUserService, UserService>();
+
             // Register future repositories:
             // services.AddScoped<ILocationRepository, LocationRepository>();
             // services.AddScoped<ITrackingSessionRepository, TrackingSessionRepository>();
---
+++ src/Location.Tracking.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using Location.Tracking.Application.Interfaces.Repositories;
+using Location.Tracking.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Location.Tracking.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly TrackingDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public BaseRepository(TrackingDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public async Task<T?> GetByIdAsync(Guid id) => await _dbSet.FindAsync(id);
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Delete(T entity) => _dbSet.Remove(entity);
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+
+    }
+}
