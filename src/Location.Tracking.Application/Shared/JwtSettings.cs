0ed4692c2621fa9353fd42e60cfc2e55b715fbe4
max <maksimas.volodascik@gmail.com>
2026-04-10 18:19:36 +0300

    chore: add missing files

---
+++ src/Location.Tracking.Application/Shared/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Location.Tracking.Application.Shared
+{
+    public class JwtSettings
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+        public int TokenExpiryInHours { get; set; } = 8;
+
+    }
+}
