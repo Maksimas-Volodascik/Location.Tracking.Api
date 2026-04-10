61eee73a1eaf673055d4f7f3cfeb9ef584888bdf
max <maksimas.volodascik@gmail.com>
2026-04-10 19:57:44 +0300

    feat(api): add proper result responses to successful and failed requests

--- src/Location.Tracking.Application/Shared/Result.cs
+++ src/Location.Tracking.Application/Shared/Result.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Location.Tracking.Application.Shared
 {
-    public class Result
+    public class Result<T>
     {
+        public bool IsSuccess { get; }
+        public Error? Error { get; }
+        public T? Data { get; }
 
+        protected Result(bool isSuccess, Error? error, T? data)
+        {
+            IsSuccess = isSuccess;
+            Error = error;
+            Data = data;
+        }
+
+        public static Result<T> Success(T data)
+        {
+            return new Result<T>(true, null, data);
+        }
+
+        public static Result<T> Failure(Error error)
+        {
+            return new Result<T>(false, error, default);
+        }
     }
 }
