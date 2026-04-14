dce39ac894943e6ef30564ac8188145d5ec7bd8b
max <maksimas.volodascik@gmail.com>
2026-04-14 20:51:48 +0300

    feat(api): add device getall, getbyid and delete methods

---
+++ src/Location.Tracking.Api/Controllers/DeviceController.cs
@@ -0,0 +1,54 @@
+﻿using Asp.Versioning;
+using Location.Tracking.Application.Interfaces.Services;
+using Location.Tracking.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Location.Tracking.Api.Controllers
+{
+    [ApiVersion(1)]
+    [Route("v{v:apiVersion}/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        private readonly IDeviceService _deviceService;
+        public DeviceController(IDeviceService deviceService)
+        {
+            _deviceService = deviceService;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<Device>>> GetAllDevicesAsync()
+        {
+            var devices = await _deviceService.GetAllDevicesAsync();
+
+            return Ok(devices);
+        }
+
+        [HttpGet("{uuid}")]
+        public async Task<ActionResult<Device>> GetDeviceByIdAsync(Guid uuid)
+        {
+            var device = await _deviceService.GetDeviceByIdAsync(uuid);
+
+            return Ok(device);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDeviceAsync()
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> UpdateDeviceAsync()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{uuid}")]
+        public async Task<IActionResult> DeleteDeviceAsync(Guid uuid)
+        {
+            await _deviceService.DeleteDeviceAsync(uuid);
+            return Ok();
+        }
+    }
+}
---
+++ src/Location.Tracking.Application/Interfaces/Repositories/IDeviceRepository.cs
@@ -0,0 +1,14 @@
+﻿using Location.Tracking.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Location.Tracking.Application.Interfaces.Repositories
+{
+    public interface IDeviceRepository : IBaseRepository<Device>
+    {
+        Task<IEnumerable<Device>> GetAllDevicesAsync();
+    }
+}
