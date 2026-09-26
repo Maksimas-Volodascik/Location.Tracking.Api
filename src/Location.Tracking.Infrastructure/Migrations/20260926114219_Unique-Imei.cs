c6c37655ec031b08677a3f4bb59972958a8028f1
max <maksimas.volodascik@gmail.com>
2026-09-26 19:40:50 +0300

    fix(database): add database migration for unique IMEI index - remove incorrect unit tests

---
+++ src/Location.Tracking.Infrastructure/Migrations/20260926114219_Unique-Imei.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Location.Tracking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueImei : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Devices_Imei_UserId",
+                table: "Devices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_Imei",
+                table: "Devices",
+                column: "Imei",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Devices_Imei",
+                table: "Devices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_Imei_UserId",
+                table: "Devices",
+                columns: new[] { "Imei", "UserId" },
+                unique: true);
+        }
+    }
+}
